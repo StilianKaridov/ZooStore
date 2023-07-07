e3e626ce8240656acc7f59be88a175de8035b938
StilianKaridov <karidovstilian@gmail.com>
2023-07-07 11:50:05 +0300

    Initial commit

---
+++ src/main/java/com/tinqin/zoostore/ZooStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tinqin.zoostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tinqin/zoostore/ZooStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinqin.zoostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
