6fd3808de41f49564f28f5a78757875c55740580
StilianKaridov <karidovstilian@gmail.com>
2023-07-10 16:05:45 +0300

    Created dto's for Tag create and update

---
+++ src/main/java/com/tinqin/zoostore/api/request/TagCreateRequest.java
@@ -0,0 +1,15 @@
+package com.tinqin.zoostore.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TagCreateRequest {
+
+    private String title;
+}
---
+++ src/main/java/com/tinqin/zoostore/api/response/TagUpdateResponse.java
@@ -0,0 +1,18 @@
+package com.tinqin.zoostore.api.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@Builder
+public class TagUpdateResponse {
+
+    private String title;
+}
