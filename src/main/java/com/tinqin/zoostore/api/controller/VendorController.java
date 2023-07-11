23a22205f9b63a9d0b07ccab79470a88038d4df4
StilianKaridov <karidovstilian@gmail.com>
2023-07-11 11:33:26 +0300

    Implemented create, edit Vendor

---
+++ src/main/java/com/tinqin/zoostore/api/controller/VendorController.java
@@ -0,0 +1,131 @@
+package com.tinqin.zoostore.api.controller;
+
+import com.tinqin.zoostore.api.request.VendorCreateRequest;
+import com.tinqin.zoostore.api.request.VendorUpdateNameRequest;
+import com.tinqin.zoostore.api.request.VendorUpdatePhoneRequest;
+import com.tinqin.zoostore.api.response.VendorCreateResponse;
+import com.tinqin.zoostore.api.response.VendorUpdateNameResponse;
+import com.tinqin.zoostore.api.response.VendorUpdatePhoneResponse;
+import com.tinqin.zoostore.exception.NoSuchVendorException;
+import com.tinqin.zoostore.exception.NullOrEmptyStringException;
+import com.tinqin.zoostore.exception.VendorAlreadyExistingException;
+import com.tinqin.zoostore.service.VendorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VendorController {
+
+    private static final String SUCCESSFULLY_CREATED_VENDOR_MESSAGE = "Successfully created Vendor with name: %s and phoneNumber: %s";
+
+    private final VendorService vendorService;
+
+    @Autowired
+    public VendorController(VendorService vendorService) {
+        this.vendorService = vendorService;
+    }
+
+    @PostMapping("/createVendor")
+    public ResponseEntity<String> createVendor(@RequestBody VendorCreateRequest vendorCreateRequest) {
+        String name = vendorCreateRequest.getName();
+        String phoneNumber = vendorCreateRequest.getPhoneNumber();
+
+        if (checkIfStringIsNullOrEmpty(name) || checkIfStringIsNullOrEmpty(phoneNumber)) {
+            throw new NullOrEmptyStringException();
+        }
+
+        VendorCreateResponse vendor = this.vendorService.createVendor(vendorCreateRequest);
+
+        return ResponseEntity.
+                status(201).
+                body(
+                        String.format(
+                                SUCCESSFULLY_CREATED_VENDOR_MESSAGE,
+                                vendor.getName(),
+                                vendor.getPhoneNumber()
+                        )
+                );
+    }
+
+    @PatchMapping("/updateVendorName")
+    public ResponseEntity<String> updateVendorName(
+            @RequestBody VendorUpdateNameRequest vendorUpdateRequest
+    ) {
+        String vendorOldName = vendorUpdateRequest.getOldName();
+        String vendorNewName = vendorUpdateRequest.getNewName();
+
+        if (checkIfStringIsNullOrEmpty(vendorOldName) ||
+                checkIfStringIsNullOrEmpty(vendorNewName)) {
+            throw new NullOrEmptyStringException();
+        }
+
+        StringBuilder response = new StringBuilder();
+
+        VendorUpdateNameResponse vendorWithUpdatedName = this.vendorService.
+                updateVendorName(vendorOldName, vendorNewName);
+
+        response.append(String.format(
+                        "Successfully updated vendor's name from %s to %s.",
+                        vendorWithUpdatedName.getOldName(),
+                        vendorWithUpdatedName.getNewName()
+                )
+        );
+
+        return ResponseEntity.
+                status(200).
+                body(response.toString());
+    }
+
+    @PatchMapping("/updateVendorPhone")
+    public ResponseEntity<String> updateVendorPhone(
+            @RequestBody VendorUpdatePhoneRequest vendorUpdatePhoneRequest
+    ) {
+        String vendorOldPhone = vendorUpdatePhoneRequest.getOldPhone();
+        String vendorNewPhone = vendorUpdatePhoneRequest.getNewPhone();
+
+        if (checkIfStringIsNullOrEmpty(vendorOldPhone) ||
+                checkIfStringIsNullOrEmpty(vendorNewPhone)) {
+            throw new NullOrEmptyStringException();
+        }
+
+        StringBuilder response = new StringBuilder();
+
+        VendorUpdatePhoneResponse vendorWithUpdatedPhone = this.vendorService.
+                updateVendorPhone(vendorOldPhone, vendorNewPhone);
+
+        response.append(String.format(
+                        "Successfully updated vendor's phone from %s to %s.",
+                        vendorWithUpdatedPhone.getOldPhone(),
+                        vendorWithUpdatedPhone.getNewPhone()
+                )
+        );
+
+        return ResponseEntity.
+                status(200).
+                body(response.toString());
+    }
+
+    private boolean checkIfStringIsNullOrEmpty(String str) {
+        return str == null || str.trim().equals("");
+    }
+
+    @ExceptionHandler(value = NullOrEmptyStringException.class)
+    public ResponseEntity<String> handleNullOrEmptyNameOrPhoneNumberException(NullOrEmptyStringException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = VendorAlreadyExistingException.class)
+    public ResponseEntity<String> handleExistingVendorException(VendorAlreadyExistingException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = NoSuchVendorException.class)
+    public ResponseEntity<String> handleNotExistingVendorException(NoSuchVendorException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
