387ea8ab45ba5b63d225cabe8ca92e71793cf174
StilianKaridov <karidovstilian@gmail.com>
2023-07-11 12:31:13 +0300

    Response class for delete vendor method

---
+++ src/main/java/com/tinqin/zoostore/api/response/VendorDeleteResponse.java
@@ -0,0 +1,17 @@
+package com.tinqin.zoostore.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class VendorDeleteResponse {
+
+    private String name;
+
+    private String phoneNumber;
+}
