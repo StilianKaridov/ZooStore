f723839aa918ffb8cc6843d52caca0a35eef6357
StilianKaridov <karidovstilian@gmail.com>
2023-07-12 19:29:17 +0300

    Validator/Custom made annotations

---
+++ src/main/java/com/tinqin/zoostore/utils/validator/ValidationUtils.java
@@ -0,0 +1,6 @@
+package com.tinqin.zoostore.utils.validator;
+
+public interface ValidationUtils {
+
+    <E> boolean isValid(E entity);
+}
---
+++ src/main/java/com/tinqin/zoostore/utils/validator/ValidationUtilsImpl.java
@@ -0,0 +1,21 @@
+package com.tinqin.zoostore.utils.validator;
+
+import jakarta.validation.Validation;
+
+import jakarta.validation.Validator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidationUtilsImpl implements ValidationUtils {
+
+    private final Validator validator;
+
+    public ValidationUtilsImpl() {
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();;
+    }
+
+    @Override
+    public <E> boolean isValid(E entity) {
+        return this.validator.validate(entity).isEmpty();
+    }
+}
