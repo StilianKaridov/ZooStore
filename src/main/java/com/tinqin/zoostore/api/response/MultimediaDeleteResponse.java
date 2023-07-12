ccd741e4f1d3fc7400a665a4af9fb64d1d08d188
StilianKaridov <karidovstilian@gmail.com>
2023-07-12 10:12:42 +0300

    Response classes for multimedia requests

---
+++ src/main/java/com/tinqin/zoostore/api/response/MultimediaDeleteResponse.java
@@ -0,0 +1,19 @@
+package com.tinqin.zoostore.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MultimediaDeleteResponse {
+
+    private String url;
+
+    private String publicId;
+
+    private String type;
+}
