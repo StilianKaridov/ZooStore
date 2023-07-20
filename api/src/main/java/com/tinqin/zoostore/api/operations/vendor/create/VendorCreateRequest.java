d97f3a466d86aa8b7b7380cdc07d6f3879991740
StilianKaridov <karidovstilian@gmail.com>
2023-07-20 10:02:36 +0300

    Phone number format validation for Bulgarian numbers

--- api/src/main/java/com/tinqin/zoostore/api/operations/vendor/create/VendorCreateRequest.java
+++ api/src/main/java/com/tinqin/zoostore/api/operations/vendor/create/VendorCreateRequest.java
@@ -2,6 +2,7 @@ package com.tinqin.zoostore.api.operations.vendor.create;
 
 import com.tinqin.zoostore.api.operations.base.OperationRequest;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,10 @@ public class VendorCreateRequest implements OperationRequest {
     @NotBlank(message = "The name must not be empty!")
     private String name;
 
+    @Pattern(
+            regexp = "(\\+)?(359|0)8[789]\\d{1}(|-| )\\d{3}(|-| )\\d{3}",
+            message = "Invalid phone number format!"
+    )
     @NotBlank(message = "The phone number must not be empty!")
     private String phoneNumber;
 }
--- api/src/main/java/com/tinqin/zoostore/api/operations/vendor/update/phone/VendorUpdatePhoneRequest.java
+++ api/src/main/java/com/tinqin/zoostore/api/operations/vendor/update/phone/VendorUpdatePhoneRequest.java
@@ -2,6 +2,7 @@ package com.tinqin.zoostore.api.operations.vendor.update.phone;
 
 import com.tinqin.zoostore.api.operations.base.OperationRequest;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,9 +17,17 @@ import lombok.Setter;
 @Builder
 public class VendorUpdatePhoneRequest implements OperationRequest {
 
+    @Pattern(
+            regexp = "(\\+)?(359|0)8[789]\\d{1}(|-| )\\d{3}(|-| )\\d{3}",
+            message = "Invalid phone number format!"
+    )
     @NotBlank(message = "The old phone must not be empty!")
     private String oldPhone;
 
+    @Pattern(
+            regexp = "(\\+)?(359|0)8[789]\\d{1}(|-| )\\d{3}(|-| )\\d{3}",
+            message = "Invalid phone number format!"
+    )
     @NotBlank(message = "The new phone must not be empty!")
     private String newPhone;
 }
