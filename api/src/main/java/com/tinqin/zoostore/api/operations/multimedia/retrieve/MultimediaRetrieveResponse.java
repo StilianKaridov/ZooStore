bbcf6238dd2dddbdcf28b8ca8595ac1426a6ca8f
StilianKaridov <karidovstilian@gmail.com>
2023-07-27 12:50:59 +0300

    Added @Builder

--- api/src/main/java/com/tinqin/zoostore/api/operations/multimedia/retrieve/MultimediaRetrieveResponse.java
+++ api/src/main/java/com/tinqin/zoostore/api/operations/multimedia/retrieve/MultimediaRetrieveResponse.java
@@ -1,15 +1,18 @@
 package com.tinqin.zoostore.api.operations.multimedia.retrieve;
 
 import com.tinqin.zoostore.api.operations.base.OperationResponse;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
-@Setter
+@Setter(AccessLevel.PRIVATE)
+@Builder
 public class MultimediaRetrieveResponse implements OperationResponse {
 
     private String url;
